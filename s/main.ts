ab9233c3b6b5d9c6b3c59fadb936434df5419515
Chase Moskal <chasemoskal@gmail.com>
2022-08-06 11:06:03 -0700

    add: wat and wasm

--- s/main.ts
+++ s/main.ts
@@ -1,9 +1,17 @@
 
 import {greet, sum} from "../rust/pkg/rust.js"
 
-greet("Chase", (...args: any[]) => console.log(...args))
+{
+	console.log("RUST:")
+	greet("Chase", (...args: any[]) => console.log(...args))
+	const a = 2
+	const b = 3
+	console.log("sum of a and b:", sum(a, b))
+}
 
-const a = 2
-const b = 3
-
-console.log("sum of a and b:", sum(a, b))
+{
+	console.log("WASM:")
+	const wasm = await WebAssembly.instantiateStreaming(fetch("/x/example.wasm"), {})
+	const {add} = <any>wasm.instance.exports
+	console.log("wasm add:", add(2, 3))
+}
